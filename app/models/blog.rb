fb8b58f25e31e9731a5801f7fba96e2a7e14769b
Marc Delagrammatikas <delagrammatikas@gmail.com>
2017-07-13 19:13:29 -0700

    Add blog controller index and make that the homepage.

---
+++ app/models/blog.rb
@@ -0,0 +1,10 @@
+# Represents the blog, which will have posts, authors, etc.
+class Blog
+  def title
+    'Watching Paint Dry'
+  end
+
+  def subtitle
+    'The trusted source for drying paint new and opinion'
+  end
+end
