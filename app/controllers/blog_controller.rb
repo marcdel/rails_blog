a0c0651566b4c533f181500cc887c7d283306dfe
Marc Delagrammatikas <delagrammatikas@gmail.com>
2017-07-15 16:16:00 -0700

    Making the blog object into a singleton

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,15 +1,4 @@
 # Lists all blog posts
 class BlogController < ApplicationController
-  def index
-    @blog = Blog.new
-
-    post1 = @blog.new_post
-    post1.title = 'Paint just applied'
-    post1.body = 'Paint just applied etc. etc.'
-    post1.publish
-
-    post2 = @blog.new_post(title: 'Still wet')
-    post2.body = 'Paint is still wet.'
-    post2.publish
-  end
+  def index; end
 end
