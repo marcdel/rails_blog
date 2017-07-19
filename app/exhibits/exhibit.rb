bb0a33ea8f541e27ed6c471c84cb166a3d31103b
Marc Delagrammatikas <delagrammatikas@gmail.com>
2017-07-18 20:57:46 -0700

    Refactoring #exhibit: simplify helper object

--- app/exhibits/exhibit.rb
+++ app/exhibits/exhibit.rb
@@ -7,6 +7,28 @@ class Exhibit < SimpleDelegator
     super(model)
   end
 
+  def self.exhibit(model, context)
+    exhibits.reduce(model) do |object, exhibit|
+      exhibit.exhibit_if_applicable(object, context)
+    end
+  end
+
+  def self.exhibit_if_applicable(object, context)
+    if applicable_to?(object)
+      new(object, context)
+    else
+      object
+    end
+  end
+
+  def self.applicable_to?(_object)
+    false
+  end
+
+  def self.exhibits
+    [TextPostExhibit, PicturePostExhibit]
+  end
+
   def to_model
     __getobj__
   end
